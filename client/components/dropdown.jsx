f491037fd97e0007c6bfd521ed2131865470aa9b
Alex Orlova <akad.alex@gmail.com>
2018-01-29 21:58:54 -0800

    Fix for edit task component

--- client/components/dropdown.jsx
+++ client/components/dropdown.jsx
@@ -3,18 +3,20 @@ import React from 'react';
 export default class Dropdown extends React.Component{
   constructor(props){
     super(props);
-    this.state={};
+    this.state={ selected: props.selected};
     this.onChange = this.onChange.bind(this);
   }
   onChange(e){
-    this.props.onChange(e.target.value);
+    let value = e.target.value;
+    this.props.onChange(value);
+    this.setState({selected: value});
   }
   render(){
     let { items, selected, idField } = this.props;
     idField = idField || 'id';
     let options = items.map(el => <option key={el[idField]}>{el.name}</option>);
 
-    return <select onChange={this.onChange} value={selected}>
+    return <select onChange={this.onChange} value={this.state.selected}>
       {options}
     </select>
   }
