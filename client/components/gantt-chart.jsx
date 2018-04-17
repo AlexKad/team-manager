c9f18319a8d4859d0773d43aee79897d1274183a
Alex Orlova <akad.alex@gmail.com>
2018-04-16 21:35:17 -0700

    Add styles for the gantt-chart component

---
+++ client/components/gantt-chart.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
+import { Tasks, Iterations } from '../../imports/collections.js';
+import { types, statuses, priorities } from '../../imports/constants.js';
+import Task from './task';
+import Dropdown from './dropdown';
+
+ class GanttChart extends React.Component{
+  constructor(props){
+    super(props);
+
+  }
+  render(){
+    return <div className='gantt-chart'>
+        <div className='input-group'>
+          <label>Start from:</label>
+          <input type='date'/> -
+          <label>Till:</label>
+          <input type='date'/>
+        </div>
+        <div className='chart-panel'>
+
+        </div>
+        <div className='tasks'>
+
+        </div>
+    </div>
+  }
+}
+
+export default withTracker(props=>{
+  Meteor.subscribe('Tasks');
+  Meteor.subscribe('Tags');
+  Meteor.subscribe('Meteor.users');
+  let user = Meteor.users.findOne({ _id: Meteor.userId() });
+
+  return {};
+
+})(GanttChart)
