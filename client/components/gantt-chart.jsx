c7987cd6c8f83f291e436f5fe6702f8169a174a3
Alex Orlova <akad.alex@gmail.com>
2018-05-04 23:02:50 -0700

    Set dates for gantt-chart

--- client/components/gantt-chart.jsx
+++ client/components/gantt-chart.jsx
@@ -8,15 +8,39 @@ import Dropdown from './dropdown';
  class GanttChart extends React.Component{
   constructor(props){
     super(props);
+    this.state = { }
+  }
+  dateChanged(val, field){
+    let { start, end } = this.state;
+    if(!val){
+      let obj = {};
+      obj[field] = '';
+      obj.dateError = ''
+      this.setState(obj);
+      return;
+    }
+    let date = new Date(val);
+    date.setDate(date.getUTCDate());
+    date = date.getTime();
 
+    if(field == 'end' && start && start>date){
+      this.setState({dateError: 'Start date should be earlier then end date'});
+      return;
+    }
+    let obj = {};
+    obj[field] = date;
+    obj.dateError = '';
+    this.setState(obj);
   }
   render(){
+    let { dateError } = this.state;
     return <div className='gantt-chart'>
         <div className='input-group'>
           <label>Start from:</label>
-          <input type='date'/>
+          <input type='date' onChange={(e)=>this.dateChanged(e.target.value, 'start')}/>
           <label> till:</label>
-          <input type='date'/>
+          <input type='date' onChange={(e)=>this.dateChanged(e.target.value, 'end')}/>
+          <span className='error'>{ dateError }</span>
         </div>
         <div className='chart-panel'>
 
